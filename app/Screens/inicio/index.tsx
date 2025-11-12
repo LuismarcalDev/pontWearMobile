ecb336df038f735525fbfe785fbebdd77901010a
luis <luisfelippe55482@gmail.com>
2025-11-12 10:44:49 -0300

    download finalizado

--- app/Screens/inicio/index.tsx
+++ app/Screens/inicio/index.tsx
@@ -8,11 +8,18 @@ import {
   ScrollView,
   Dimensions,
 } from "react-native";
+
 import { Ionicons } from "@expo/vector-icons";
+import * as MediaLibrary from 'expo-media-library';
+
+import * as FileSystem from 'expo-file-system/legacy';
+
 
 export default function Inicio() {
+
   const [coluna1, setColuna1] = useState([]);
   const [coluna2, setColuna2] = useState([]);
+  const [dados, setDados] = useState<any | null>(null);
 
   useEffect(() => {
     async function carregar() {
@@ -21,8 +28,8 @@ export default function Inicio() {
       );
       const data = await res.json();
 
-      const esquerda: string[] = [];
-      const direita: string[] = [];
+      const esquerda: any[] = [];
+      const direita: any[] = [];
       data.forEach((img, i) => {
         if (i % 2 === 0) esquerda.push(img);
         else direita.push(img);
@@ -37,10 +44,28 @@ export default function Inicio() {
 
   const largura = Dimensions.get("window").width / 2 - 9;
 
+  function pegar(id: string) {
+    solo(id);
+    console.log(id);
+  }
+
+  async function solo(id: string) {
+    const res = await fetch(
+      `https://api.unsplash.com/photos/${id}?client_id=n716mTyoxj5RTGdu0b3V0yX9RLpIlIEsHhhmEE1Znjo`
+    );
+    const data = await res.json();
+    setDados(data);
+  }
+
+async function baixar(url) {
+  const { uri } = await FileSystem.downloadAsync(url, FileSystem.documentDirectory + 'foto.jpg');
+  await MediaLibrary.saveToLibraryAsync(uri);
+  console.log('Salvo na galeria!');
+}
   return (
     <View style={estilos.geral}>
       <View style={estilos.header}>
-        <Text style={estilos.title}>WIKI | FILMES</Text>
+        <Text style={estilos.title}>PONT | WEAR</Text>
         <Ionicons name="search" size={25} color="#ffffff" />
       </View>
 
@@ -49,7 +74,11 @@ export default function Inicio() {
           {/* Coluna esquerda */}
           <View style={estilos.coluna}>
             {coluna1.map((item, i) => (
-              <TouchableOpacity key={i} activeOpacity={0.8}>
+              <TouchableOpacity
+                key={i}
+                activeOpacity={0.8}
+                onPress={() => pegar(item.id)}
+              >
                 <Image
                   source={{ uri: item.urls.small }}
                   style={[
@@ -64,9 +93,13 @@ export default function Inicio() {
           {/* Coluna direita */}
           <View style={estilos.coluna}>
             {coluna2.map((item, i) => (
-              <TouchableOpacity key={i} activeOpacity={0.8}>
+              <TouchableOpacity
+                key={i}
+                activeOpacity={0.8}
+                onPress={() => pegar(item.id)}
+              >
                 <Image
-                  source={{ uri: item.urls.small }}
+                  source={{ uri: item.urls.full }}
                   style={[
                     estilos.image,
                     { width: largura, height: 220 + ((i + 1) % 2) * 80 },
@@ -77,11 +110,64 @@ export default function Inicio() {
           </View>
         </View>
       </ScrollView>
-      <View style={estilos.sr}>
-            <View style={estilos.cr}>
-                <View></View>
+
+      {dados && (
+        <TouchableOpacity
+          style={estilos.sr}
+          activeOpacity={1}
+          onPress={() => setDados(null)}
+        >
+          <View style={estilos.cr}>
+            <Image
+              source={{
+                uri: dados.urls?.small,
+              }}
+              style={estilos.kr}
+            />
+
+            <View style={estilos.sqr}>
+              <View
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  gap: "5px",
+                  flexDirection: "row",
+                  paddingBottom: 20,
+                }}
+              >
+                <View
+                  style={{
+                    width: 15,
+                    height: 15,
+                    backgroundColor: "#c40c0c",
+                    borderRadius: "100px",
+                  }}
+                ></View>
+                <View
+                  style={{
+                    width: 15,
+                    height: 15,
+                    backgroundColor: "#0cc40c",
+                    borderRadius: "100px",
+                  }}
+                ></View>
+              </View>
+
+              <TouchableOpacity onPress={() => baixar(dados.urls.small)}>
+                <Ionicons
+                  name="cloud-download-outline"
+                  size={29}
+                  color="black"
+                />
+              </TouchableOpacity>
+
+              <TouchableOpacity>
+                <Ionicons name="heart" size={29} color="#494949" />
+              </TouchableOpacity>
             </View>
-      </View>
+          </View>
+        </TouchableOpacity>
+      )}
     </View>
   );
 }
@@ -111,30 +197,42 @@ const estilos = StyleSheet.create({
   },
   coluna: {
     flexDirection: "column",
-    gap: 30, // espaçamento entre imagens
+    gap: 30,
   },
   image: {
     borderRadius: 10,
     resizeMode: "cover",
   },
-  sr:{
-    position:"absolute",
-    width:"100%",
-    height:"100%",
-    backgroundColor:"#0f0f0f46",
-    display:"flex",
-    flexDirection:"column",
-    alignItems:"center",
-    
-
+  sr: {
+    position: "absolute",
+    width: "100%",
+    height: "110%",
+    backgroundColor: "#0f0f0f46",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    top:-50
   },
-  cr:{
-    width:300,
-    height:370,
-    backgroundColor:"white",
-    top:100,
-    borderRadius:5
-
-  }
-
+  cr: {
+    width: 320,
+    height: 370,
+    backgroundColor: "white",
+    borderRadius: 5,
+    display: "flex",
+    flexDirection: "row",
+  },
+  kr: {
+    width: "85%",
+    height: "100%",
+  },
+  sqr: {
+    width: "15%",
+    height: "100%",
+    display: "flex",
+    alignItems: "center",
+    flexDirection: "column",
+    paddingTop: 10,
+    gap: 10,
+  },
 });
