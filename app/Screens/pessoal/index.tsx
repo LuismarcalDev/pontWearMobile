3cf43998d42cdabd8724371904b5a3c7d7d79028
luis <luisfelippe55482@gmail.com>
2025-11-14 15:27:09 -0300

    começo da pag pessoal

--- app/Screens/pessoal/index.tsx
+++ app/Screens/pessoal/index.tsx
@@ -3,12 +3,15 @@ import {
   View,
   Text,
   StyleSheet,
-  TextInput
+  TextInput,
+  Image,
+  TouchableOpacity,
+  ImageBackground,
 
 } from "react-native";
 
 import { Ionicons } from "@expo/vector-icons";
-
+//https://images.pexels.com/photos/1853542/pexels-photo-1853542.jpeg?_gl=1*1ptcsd4*_ga*ODMxNjgzNDgzLjE3NjI5NzU1MjQ.*_ga_8JE65Q40S6*czE3NjMxNDA4MTEkbzIkZzEkdDE3NjMxNDA4NDgkajIzJGwwJGgw
 export default function Pessoal() {
 
 
@@ -30,11 +33,33 @@ export default function Pessoal() {
       </View>
 
 
-    <View style={estilos.fundo}>
+     <ImageBackground
+      source={{ uri: 'https://images.pexels.com/photos/1853542/pexels-photo-1853542.jpeg' }}
+      style={ estilos.fundo}
+    >
+      <View style={estilos.ahh}>
+        <TouchableOpacity style={estilos.imgs}>
+           <Image
+           source={{ uri: "" }}
+         
+         />
+        </TouchableOpacity>
+        <TextInput placeholder="Seu Nome" style={estilos.br}/>
+        <TextInput/>
 
-   </View>
+        
+      </View>
+      </ImageBackground>
 
-
+      <View style={{display:"flex", top:50,paddingLeft:30,paddingBottom:10,paddingRight:30}}>
+        <View style={{display:"flex", flexDirection:"row", gap:5}}>
+         
+          <Text style={{color:"white", fontSize:20,}}>
+            Seus Favoritos
+          </Text>
+           <Ionicons name="heart" size={29} color="#474646" />
+        </View>
+      </View>
 
 
 
@@ -53,7 +78,9 @@ const estilos = StyleSheet.create({
     justifyContent: "space-between",
     paddingHorizontal: 15,
     alignItems: "center",
-    padding:12
+    padding:12,
+    paddingLeft:30,
+    paddingRight:30
   },
   title: {
     color: "white",
@@ -65,9 +92,36 @@ const estilos = StyleSheet.create({
   },
   fundo:{
     width:"100%",
-    height:200,
-    backgroundColor:"red"
+    height:180,
+
+ paddingTop:10,
+ paddingLeft:30,
+ paddingBottom:10,
+ paddingRight:30
+
+
+  },
+  imgs:{
+    width:80,
+    height:80,
+    backgroundColor:"white",
+    borderRadius:100
+    
+  },
+  br:{
+    color:"white",
+    fontSize:22,
+    
+
+  },
+  ahh:{
+ display:"flex",
+ flexDirection:"column",
+ gap:10
   }
+
+
+
 
 
 });
